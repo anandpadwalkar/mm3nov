a9706b1c81ffef53a5ea8d830279780717b303e4
anandp <anandpadwalkar@gmail.com>
2015-11-14 13:05:26 +0530

    14nov

--- src/main/java/com/iitms/rfcampuscommon/JsonResponse.java
+++ src/main/java/com/iitms/rfcampuscommon/JsonResponse.java
@@ -1,8 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2015 MasterSoft.
- * Team Java
- * All rights reserved.
- *******************************************************************************/
 package com.iitms.rfcampuscommon;
 
 import java.io.Serializable;
@@ -11,6 +6,8 @@ public class JsonResponse implements Serializable{
 
     private boolean hasError;
     private String responseMessage;
+    private String fName;
+    private String lName;
     
     public boolean isHasError() {
         return hasError;
@@ -23,6 +20,18 @@ public class JsonResponse implements Serializable{
     }
     public void setResponseMessage(String responseMessage) {
         this.responseMessage = responseMessage;
+    }
+    public String getfName() {
+        return fName;
+    }
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+    public String getlName() {
+        return lName;
+    }
+    public void setlName(String lName) {
+        this.lName = lName;
     }
     
     
